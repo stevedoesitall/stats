2539be375b843dbbe4187b29870d9d5d116a284e
Steve Giordano <stephenagiordano@gmail.com>
2019-07-21 16:57:40 -0400

    Update app_det.js

---
+++ scripts/list/app_det.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const fs = require("fs");
+const dir = __dirname;
+
+const creds = path.join(dir, "../creds.json");
+
+const api_key = require(creds).api_key;
+const api_secret = require(creds).api_secret;
+const sailthru = require("sailthru-client").createSailthruClient(api_key, api_secret);
+
